f844640b8719f8a0b7b4298bc8d3fc766018450d
Muhammad Bilal Ansari <muhammadbilalansari@hotmail.com>
2024-01-06 12:12:58 +0500

    Done with initial navbar and prisma init

---
+++ app/NavBar.tsx
@@ -0,0 +1,84 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { PiBug } from "react-icons/pi";
+
+interface LinkModel {
+  text: string;
+  url: string;
+}
+const NavBar = () => {
+  const path = usePathname();
+  console.log(path);
+  const links: LinkModel[] = [
+    { text: "Dashboard", url: "/" },
+    { text: "Tickets", url: "/tickets" },
+  ];
+  return (
+    <nav className="navbar bg-accent text-white">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-4xl">
+          <PiBug></PiBug>
+        </Link>
+        <ul className="menu menu-horizontal px-1">
+          {links.map((link) => (
+            <li className="p-1" key={link.text}>
+              <Link
+                className={classNames({
+                  active: path === link.url,
+                })}
+                href={link.url}
+              >
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;

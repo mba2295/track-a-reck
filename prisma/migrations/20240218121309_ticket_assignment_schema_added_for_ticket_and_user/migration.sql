300dcbc25e9c7f508566aa83f29f29cc0c234912
Muhammad Bilal Ansari <muhammadbilalansari@hotmail.com>
2024-03-17 17:07:08 +0500

    updated the schema and migrations

--- prisma/migrations/20240218121309_ticket_assignment_schema_added_for_ticket_and_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `ticket` ADD COLUMN `assignedToId` VARCHAR(255) NULL;
-
--- CreateIndex
-CREATE INDEX `idx_assignedToId` ON `Ticket`(`assignedToId`);
-
--- AddForeignKey
-ALTER TABLE `Ticket` ADD CONSTRAINT `Ticket_assignedToId_fkey` FOREIGN KEY (`assignedToId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
